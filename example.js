0d30845204e3a7090ac666408031aae6e86d12de
Pankaj Kumar <pxkumar@liquidhub.com>
2017-03-07 23:44:04 +0530

    commit bubhdsifuhsda

--- example.js
+++ example.js
@@ -6,24 +6,7 @@ var express = require("express"),
 var FitbitApiClient = require("fitbit-node"),
     client = new FitbitApiClient("228BSX", "6a1421682320690a85c85b78c6e59f15");
 
-// redirect the user to the Fitbit authorization page
-app.get("/authorize", function (req, res) {
-    // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://tranquil-gorge-60872.herokuapp.com/'));
-});
-
-// handle the callback from the Fitbit authorization flow
-app.get("/callback", function (req, res) {
-    // exchange the authorization code we just received for an access token
-    client.getAccessToken(req.query.code, 'https://tranquil-gorge-60872.herokuapp.com/').then(function (result) {
-        // use the access token to fetch the user's profile information
-        client.get("/profile.json", result.access_token).then(function (results) {
-            res.send(results[0]);
-        });
-    }).catch(function (error) {
-        res.send(error);
-    });
-});
-
 // launch the server
-app.listen(3000);
+app.listen(3000, ()=>{
+	console.log('server started on port --> 30000');
+});
