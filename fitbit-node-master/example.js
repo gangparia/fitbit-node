14be910acf7c2f00b8fd391c4a0aeddc6c54f1ce
Pankaj Kumar <pxkumar@liquidhub.com>
2017-03-07 15:55:34 +0530

    changes

--- fitbit-node-master/example.js
+++ fitbit-node-master/example.js
@@ -4,18 +4,18 @@ var express = require("express"),
 
 // initialize the Fitbit API client
 var FitbitApiClient = require("fitbit-node"),
-    client = new FitbitApiClient("228BSX", "6a1421682320690a85c85b78c6e59f15");
+    client = new FitbitApiClient("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET");
 
 // redirect the user to the Fitbit authorization page
 app.get("/authorize", function (req, res) {
     // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://tranquil-gorge-60872.herokuapp.com/'));
+    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://damp-cove-60387.herokuapp.com/'));
 });
 
 // handle the callback from the Fitbit authorization flow
 app.get("/callback", function (req, res) {
     // exchange the authorization code we just received for an access token
-    client.getAccessToken(req.query.code, 'https://tranquil-gorge-60872.herokuapp.com/').then(function (result) {
+    client.getAccessToken(req.query.code, 'https://damp-cove-60387.herokuapp.com/').then(function (result) {
         // use the access token to fetch the user's profile information
         client.get("/profile.json", result.access_token).then(function (results) {
             res.send(results[0]);
